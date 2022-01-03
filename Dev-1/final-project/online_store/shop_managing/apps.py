fc7f186ed3cc750855f399acc67ef4356ed4b176
rbehzad <rbehzad1001@gmail.com>
2022-01-03 23:34:12 +0330

    remove directory

--- Dev-1/final-project/online_store/shop_managing/apps.py
+++
@@ -1,6 +0,0 @@
-from django.apps import AppConfig
-
-
-class ShopManagingConfig(AppConfig):
-    default_auto_field = 'django.db.models.BigAutoField'
-    name = 'shop_managing'
--- Dev-1/final-project/online_store/shop_managing/filters.py
+++
@@ -1,8 +0,0 @@
-import django_filters
-from .models import *
-
-
-class ShopFilter(django_filters.FilterSet):
-    class Meta:
-        model = Shop
-        fields = ['status', 'shop_type']
