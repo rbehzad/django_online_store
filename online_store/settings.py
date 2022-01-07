9578dc5edbbef931eeba0079eca3a7c2a3010c2a
rbehzad <rbehzad1001@gmail.com>
2022-01-07 17:00:36 +0330

    debug

--- online_store/settings.py
+++ online_store/settings.py
@@ -70,7 +70,7 @@ TEMPLATES = [
                 'django.template.context_processors.request',
                 'django.contrib.auth.context_processors.auth',
                 'django.contrib.messages.context_processors.messages',
-                'shop_managing.views.shop_base',
+                # 'shop_managing.views.shop_base',
             ],
         },
     },
